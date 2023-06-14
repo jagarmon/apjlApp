2d9b2656b3ea47e007d64d514b1939fdc7ecd4ec
jagarmon <xaviercin17@gmail.com>
2023-06-14 14:57:58 +0200

    commit setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackButtonComponent } from './back-button/back-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackButtonComponent
   ],
   imports: [
     BrowserModule,
--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { BackButtonComponent } from '../back-button/back-button.component';
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.css']
 })
 export class InventoryComponent {
-
+  
 }
